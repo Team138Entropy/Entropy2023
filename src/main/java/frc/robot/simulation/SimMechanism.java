1e6b2016c138738f6dff3509cb8e9ee867ac2668
joe <jacaraccio@gmail.com>
2023-02-03 19:21:52 -0500

    sim mech

--- src/main/java/frc/robot/simulation/SimMechanism.java
+++ src/main/java/frc/robot/simulation/SimMechanism.java
@@ -20,6 +20,13 @@ public class SimMechanism {
     private MechanismLigament2d mTower;
     private MechanismLigament2d mArm;
 
+    private Mechanism2d mGrasperMech;
+    private MechanismRoot2d mGrasperRootRight;
+    private MechanismRoot2d mGrasperRootLeft;
+    private MechanismLigament2d mGrasperConnection;
+    private MechanismLigament2d mLeftClaw;
+    private MechanismLigament2d mRightClaw;
+
     private SimMechanism()
     {
         // Init Mechanism 
@@ -38,7 +45,23 @@ public class SimMechanism {
 
         // Arm which pivots on top of the tower
         mArm = mArmRoot.append(new MechanismLigament2d("arm", 1, 45));
+        
 
+        // Grasper Mechanism Canvas
+        mGrasperMech = new Mechanism2d(9, 9); //Canvas Size (10,10)
+
+        // Grasper Mechanism Root (Right Side)
+        mGrasperRootRight = mGrasperMech.getRoot("grasper root right", 7, 2); 
+        // Grasper Mechanism Root (Left Side)
+        mGrasperRootLeft = mGrasperMech.getRoot("grasper root left", 2, 2); 
+
+        // Grasper Connection Ligament (Left Side)
+        mGrasperConnection = mGrasperRootLeft.append(new MechanismLigament2d("Grasper Connection", 5, 0));
+        // Left Grasper Ligament
+        mLeftClaw = mGrasperRootLeft.append(new MechanismLigament2d("Left Claw", 5, 90));
+        // Right Grasper Ligament
+        mRightClaw = mGrasperRootRight.append(new MechanismLigament2d("Right Claw", 5, 90));
+        
 
         // Useful Methods on mArm:
         //mArm.setAngle(0);
@@ -63,8 +86,29 @@ public void SetArmLength (double length){
         }
     }
 
+    
+public void simGrasperState (double angle){
+    if(angle <= 0 && angle >= -360){
+        angle = angle +360;
+
+    }
+        
+    if(angle <= 230 || angle >= 310){
+        mArm.setAngle(angle);
+    }
+}
+
+/*
+if (mOperatorInterface.getSimGrasperClosed()){
+    mGrasper.setSimGrasperClosed();
+}
+*/
+
+// make it so the mode changes stuff not buttons
+
     public void updateSmartDashboard() {
         final String key = "SimMechanism/";
         SmartDashboard.putData(key + "Arm Mechanism", mArmMech);
+        SmartDashboard.putData(key + "Grasper Mechanism", mGrasperMech);
     }
 }
