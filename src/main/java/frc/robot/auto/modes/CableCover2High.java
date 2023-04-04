1d567f7dbd5e0fd95f4776c5f6d0c29c368bd316
george <george.bogonis@student39.org>
2023-04-03 20:44:53 -0400

    cable cover auto stuff

--- src/main/java/frc/robot/auto/modes/CableCover2High.java
+++ src/main/java/frc/robot/auto/modes/CableCover2High.java
@@ -64,7 +64,7 @@ public class CableCover2High extends AutoModeBase {
 
       // Tell the Arm to go to the Backside Intake while you slowly drive
       DriveTrajectoryAction scoreToGp1TrajectoryAction = new DriveTrajectoryAction(
-        SwerveRotation.FRONT_FACING_GRID.getRotation(), 1.2, 1.8);
+        SwerveRotation.FRONT_FACING_GRID.getRotation(), 1.4, 1.8);
         scoreToGp1TrajectoryAction.addPose(
             new Pose2d(
                 ScoreSpot1,
@@ -79,6 +79,7 @@ public class CableCover2High extends AutoModeBase {
                 SwerveRotation.FRONT_FACING_GRID.getRotation()
             )
         );
+        scoreToGp1TrajectoryAction.setEndVelocity(0);
         scoreToGp1TrajectoryAction.generate();
         incrimentDuration(scoreToGp1TrajectoryAction.getEstimatedDuration());
 
@@ -99,7 +100,7 @@ public class CableCover2High extends AutoModeBase {
 
         // Drive back!
         DriveTrajectoryAction Gp1ToScoreSpot2 = new DriveTrajectoryAction(
-        SwerveRotation.FRONT_FACING_GRID.getRotation(), 1.2, 1.8);
+        SwerveRotation.FRONT_FACING_GRID.getRotation(), 2, 1.8);
         Gp1ToScoreSpot2.addPose(
             new Pose2d(
                 Stage4.plus(new Translation2d(0,0)),
@@ -110,11 +111,13 @@ public class CableCover2High extends AutoModeBase {
         Gp1ToScoreSpot2.addTranslation(CS_LowerEntrance);
         Gp1ToScoreSpot2.addPose(
             new Pose2d(
-                ScoreSpot2,
+                ScoreSpot2.plus(new Translation2d(-.52,.1)),
                 SwerveRotation.FRONT_FACING_GRID.getRotation()
             )
         );
+        Gp1ToScoreSpot2.setEndVelocity(0);
         Gp1ToScoreSpot2.generate();
+       // Gp1ToScoreSpot2.useVision();
         incrimentDuration(Gp1ToScoreSpot2.getEstimatedDuration());
 
         addAction(
@@ -128,6 +131,7 @@ public class CableCover2High extends AutoModeBase {
         addAction(new WaitAction(.1));
         addAction(new GrasperAction(true));
 
+        //addAction(new ArmAction(ArmTargets.HOME_BACKSIDE));
 
 
     }
