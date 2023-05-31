3c89c1f30b934f02d479ef87b4cf4a6e13fbd835
george <george.bogonis@student39.org>
2023-05-31 19:04:14 -0400

    advantage kit intergraded

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,5 +1,6 @@
 package frc.robot;
 
+import org.littletonrobotics.junction.LoggedRobot;
 import org.photonvision.SimVisionTarget;
 
 import edu.wpi.first.apriltag.AprilTag;
@@ -47,7 +48,7 @@ import frc.robot.subsystems.Grasper.GrasperState;
  * the package after creating this project, you must also update the build.gradle file in the
  * project.
  */
-public class Robot extends TimedRobot {
+public class Robot extends LoggedRobot {
   NetworkTableInstance inst = NetworkTableInstance.getDefault();
 
 
